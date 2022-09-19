d7bd7938d9ee6898d202e6a70a9afb3b212fe64d
islammd39e <islammdrahat308@gmail.comm>
2022-09-19 18:02:32 +0600

    dynamic country work

--- main.js
+++ main.js
@@ -1,1 +1,26 @@
-console.log('libge');
+const countrys = () => {
+    fetch("https://restcountries.com/v3.1/all")
+        .then(res => res.json())
+        .then(data => loadData(data))
+    // console.log(data[0].capital);
+}
+
+
+const loadData = (countrys) => {
+    // console.log(countrys);
+    const allCountry = countrys.map(country => getCountry(country))
+    // console.log(countrys[0]);
+    const display = document.getElementById('display')
+    display.innerHTML = allCountry.join(' ')
+}
+const getCountry = country => {
+    return `
+          <div class = "country">
+              <h2>${country.name.common}</h2>
+              <img src = "${country.flags.png}">
+          </div>    
+
+    `
+}
+// loadData()
+countrys()
